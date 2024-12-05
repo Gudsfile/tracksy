2221a927892f19f3345fd56d89a0ee5d3deffe12
bbourgeois <me@bbourgeois.dev>
2024-12-05 14:53:18 +0100

    :art: configure prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+
+export default {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
