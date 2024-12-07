a9fdf99284575d5b75abda25b5c4f6a7466ddfe5
Tarektouati <19335073+Tarektouati@users.noreply.github.com>
2024-12-07 13:50:01 +0100

    isolate query methods

---
+++ src/db/queries/queryDB.ts
@@ -0,0 +1,26 @@
+import { getDB } from '../getDB'
+import { TABLE } from './constants'
+
+import * as arrow from 'apache-arrow'
+
+const TRACK_METRICS_BY_DATE = `
+SELECT
+  ms_played,
+  ts::date as ts,
+  username
+FROM ${TABLE}
+order by ts
+`
+
+type QueryResult = {
+    ms_played: arrow.Float
+    ts: arrow.Date_
+    username: arrow.Utf8
+}
+
+export async function queryDB() {
+    const { conn } = await getDB()
+
+    const results = await conn.query<QueryResult>(TRACK_METRICS_BY_DATE)
+    return results
+}
