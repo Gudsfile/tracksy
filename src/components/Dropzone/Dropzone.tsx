90d4823a2136c8ade0e918a3ff4d361e982b2cea
Tarektouati <19335073+Tarektouati@users.noreply.github.com>
2024-12-12 15:06:29 +0100

    restrict upload to ZIP and JSON content-type

--- src/components/Dropzone/Dropzone.tsx
+++ src/components/Dropzone/Dropzone.tsx
@@ -14,20 +14,25 @@ export const Dropzone = ({
             <div
                 className="flex flex-col items-center justify-center p-6 border-2 border-dashed rounded-lg bg-gray-100 hover:bg-gray-200 transition-all cursor-pointer"
                 onDrop={handleDrop}
+                aria-label="dropzone"
                 onDragOver={handleDragOver}
             >
                 <input
                     type="file"
                     className="hidden"
                     id="fileInput"
+                    aria-label="upload file"
                     onChange={handleFileUpload}
+                    accept=".zip,application/json"
                 />
                 <label
                     htmlFor="fileInput"
-                    className="text-gray-600 text-sm cursor-pointer"
+                    className="text-gray-600 text-sm cursor-pointer text-center"
                 >
-                    Glissez & déposez des fichiers ici, ou cliquez pour en
-                    sélectionner
+                    Drag and drop or click to upload your files
+                    <br />
+                    Only <strong>ZIP archive</strong> or a{' '}
+                    <strong>JSON file</strong> are accepted
                 </label>
             </div>
         </div>
