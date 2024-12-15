5442db4408477288190d202044bb7dca3f6e71c7
Tarektouati <19335073+Tarektouati@users.noreply.github.com>
2024-12-15 18:22:06 +0100

    add missing DataTransferMock in JSDOM

--- vitest.setupFiles.ts
+++ vitest.setupFiles.ts
@@ -1,6 +1,13 @@
 import { cleanup } from '@testing-library/react'
 
-import { afterEach } from 'vitest'
+import { afterEach, beforeEach } from 'vitest'
+
+import { DataTransferMock } from './src/test-utils/DataTransferMock'
+
+beforeEach(() => {
+    // @ts-expect-error DataTransfer is not defined in jsdom
+    global.DataTransfer = DataTransferMock
+})
 
 afterEach(() => {
     cleanup()
