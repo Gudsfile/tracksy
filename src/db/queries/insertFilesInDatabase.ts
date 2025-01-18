b038293f0ea5c01f919abd13df9bc81dce6e85f0
Tarektouati <19335073+Tarektouati@users.noreply.github.com>
2025-01-18 21:28:41 +0100

    attempt to add a storage for duckdb

--- src/db/queries/insertFilesInDatabase.ts
+++ src/db/queries/insertFilesInDatabase.ts
@@ -4,26 +4,35 @@ import { tableFromJSON } from 'apache-arrow'
 
 const DROP_TABLE_QUERY = `DROP TABLE IF EXISTS ${TABLE}`
 
-export async function insertFilesInDatabase(files: FileList) {
-    if (files.length < 1) {
-        console.error('No data')
-        throw new Error('No data to process')
+export async function insertFilesInDatabase(files: FileList | undefined) {
+    const { conn } = await getDB()
+    let arrayOfFilesContents: Record<string, unknown>[] = []
+
+    if (sessionStorage.getItem('table') === null) {
+        if ((files ?? []).length < 1) {
+            console.error('No data')
+            throw new Error('No data to process')
+        }
+
+        await conn.query(DROP_TABLE_QUERY)
+        console.debug(`Table ${TABLE} dropped.`)
+
+        arrayOfFilesContents = (
+            await Promise.all(
+                Array.from(files!).map(async (file) => {
+                    console.debug(`File ${file.name} is being processed.`)
+                    const rawContent = await file.text()
+                    return JSON.parse(rawContent)
+                })
+            )
+        ).flat()
+
+        sessionStorage.setItem('table', JSON.stringify(arrayOfFilesContents))
+    } else {
+        arrayOfFilesContents = JSON.parse(sessionStorage.getItem('table')!)
     }
 
-    const { conn } = await getDB()
-
-    await conn.query(DROP_TABLE_QUERY)
-    console.debug(`Table ${TABLE} dropped.`)
-
-    const arrayOfFilesContents: Record<string, unknown>[] = await Promise.all(
-        Array.from(files).map(async (file) => {
-            console.debug(`File ${file.name} is being processed.`)
-            const rawContent = await file.text()
-            return JSON.parse(rawContent)
-        })
-    )
-
-    const arrowTableContent = tableFromJSON(arrayOfFilesContents.flat())
+    const arrowTableContent = tableFromJSON(arrayOfFilesContents)
     await conn.insertArrowTable(arrowTableContent, {
         name: TABLE,
         create: true,
