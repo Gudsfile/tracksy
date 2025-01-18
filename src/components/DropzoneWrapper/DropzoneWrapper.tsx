f828ae9ad49527d511898e89067c16bfd312e0d5
Tarektouati <19335073+Tarektouati@users.noreply.github.com>
2025-01-18 16:51:03 +0100

    refactor DropzoneWrapper with intermediate hook

--- src/components/DropzoneWrapper/DropzoneWrapper.tsx
+++ src/components/DropzoneWrapper/DropzoneWrapper.tsx
@@ -1,86 +1,20 @@
 import { useState } from 'react'
 
-import { insertFilesInDatabase } from '../../db/queries/insertFilesInDatabase'
-
 import { ChartWrapper } from '../ChartWrapper/ChartWrapper'
 import { Dropzone } from '../Dropzone/Dropzone'
-import { isAllowedFileContentType } from '../../utils/isAllowedFileContentType'
-import { isZipArchive } from '../../utils/isZipArchive'
-import { convertArrayToFileList } from '../../utils/convertArrayToFileList'
-import { openArchive } from '../../utils/openArchive'
-import { isSpotifyFilename } from '../../utils/isSpotifyFilename'
+import { useFileUpload } from './useFileUpload'
 
 export const DropzoneWrapper = () => {
     const [filesReadyToBeRequested, setFilesReadyToBeRequested] =
         useState<boolean>(false)
 
-    const controlUploadedFiles = (files: FileList) => {
-        const allowedFiles = Array.from(files).filter(isAllowedFileContentType)
-
-        if (allowedFiles.length !== files.length) {
-            throw new Error(
-                'One or more files have an unsupported content type'
-            )
-        }
-    }
-
-    const manageUploadedFiles = async (files: FileList) => {
-        setFilesReadyToBeRequested(false)
-        try {
-            controlUploadedFiles(files)
-
-            if (files.length === 1 && isZipArchive(files[0])) {
-                // TODO: Move this in a separate component, maybe in Layout
-                const archive = await openArchive(files[0])
-                let filteredFiles: File[] = []
-                const extractedFiles: Record<
-                    string,
-                    File | Record<string, File>
-                > = await archive.extractFiles()
-
-                const filteredFilesKeys = Object.keys(extractedFiles).filter(
-                    (key) => !['__MACOSX'].includes(key)
-                )
-
-                if (filteredFilesKeys.length === 0)
-                    throw new Error('No files found in the archive')
-
-                if (
-                    Object.values(extractedFiles).some(
-                        (file: File | Record<string, File>) =>
-                            typeof file === 'object' && 'type' in file
-                    )
-                ) {
-                    filteredFiles = Object.values(extractedFiles).filter(
-                        (file): file is File =>
-                            file instanceof File
-                                ? isAllowedFileContentType(file) &&
-                                  isSpotifyFilename(file)
-                                : false
-                    )
-                } else {
-                    filteredFiles = Object.values(
-                        extractedFiles[filteredFilesKeys[0]]
-                    ).filter(
-                        (file: File) =>
-                            isAllowedFileContentType(file) &&
-                            isSpotifyFilename(file)
-                    )
-                }
-
-                await insertFilesInDatabase(
-                    convertArrayToFileList(filteredFiles)
-                )
-            } else {
-                await insertFilesInDatabase(files)
-            }
-
-            setFilesReadyToBeRequested(true)
-        } catch (error) {
-            console.error('Error while processing files:', error)
+    const { uploadFiles } = useFileUpload({
+        onSuccess: () => setFilesReadyToBeRequested(true),
+        onFail: () => {
+            console.error('Upload failed')
             setFilesReadyToBeRequested(false)
-        }
-    }
+        },
+    })
 
     const handleFileUpload = async (
         event: React.ChangeEvent<HTMLInputElement>
@@ -89,8 +23,8 @@ export const DropzoneWrapper = () => {
 
         if (files === null) return undefined
 
-        console.log('Uploaded files:', Array.from(files))
-        await manageUploadedFiles(files)
+        console.debug('Uploaded files:', Array.from(files))
+        await uploadFiles(files)
     }
 
     const handleDrop = async (event: React.DragEvent<HTMLDivElement>) => {
@@ -98,8 +32,8 @@ export const DropzoneWrapper = () => {
 
         const files = event.dataTransfer.files
 
-        console.log('Dragged in files:', Array.from(files))
-        await manageUploadedFiles(files)
+        console.debug('Dragged in files:', Array.from(files))
+        await uploadFiles(files)
     }
 
     const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
@@ -112,6 +46,7 @@ export const DropzoneWrapper = () => {
                 handleDrop={handleDrop}
                 handleDragOver={handleDragOver}
                 handleFileUpload={handleFileUpload}
+                contentTypeAccepted=".zip,application/json"
             />
             {filesReadyToBeRequested && <ChartWrapper />}
         </>
