1e6e6a6d209601cafd7b4b8fabfbaea770567fbb
Gudsfile <gudsfile@protonmail.com>
2025-02-07 22:53:18 +0100

    reset components

--- app/src/components/DropzoneWrapper/DropzoneWrapper.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react'
-import { it } from 'vitest'
-
-import { DropzoneWrapper } from './DropzoneWrapper'
-
-it('should render', () => {
-    render(<DropzoneWrapper />)
-})
