9b4d5406bfb54c1ce11d4ee49ca0857b97b8fcd5
Gudsfile <gudsfile@protonmail.com>
2025-02-08 02:57:46 +0100

    show loading spinner before loading charts
    Co-authored-by: Tarektouati <19335073+Tarektouati@users.noreply.github.com>

--- app/src/components/TracksyWrapper.tsx
+++ app/src/components/TracksyWrapper.tsx
@@ -3,11 +3,12 @@ import { getDB } from '../db/getDB'
 import DropzoneWrapper from './Dropzone/DropzoneWrapper'
 import { insertFilesInDatabase } from '../db/queries/insertFilesInDatabase'
 import Charts from './Charts/Charts'
+import { Spinner } from './Spinner/Spinner'
 import type { DuckdbApp as DuckdbAppType } from '../db/setupDB'
-
 
 export default function TracksyWrapper() {
     const [db, setDb] = useState<DuckdbAppType | null>(null)
+    const [isDataDropped, setIsDataDropped] = useState(false)
     const [isDataReady, setIsDataReady] = useState(false)
 
     useEffect(() => {
@@ -22,6 +23,7 @@ export default function TracksyWrapper() {
         if (!files) return
         console.log('New dropped files:', files)
         setIsDataReady(false)
+        setIsDataDropped(true)
         await insertFilesInDatabase(files)
         setIsDataReady(true)
         console.log('New uploaded files:', files)
@@ -29,7 +31,10 @@ export default function TracksyWrapper() {
 
     return (
         <>
-            {db && <DropzoneWrapper setFiles={handleFileUpload} />}
+            {db && !(isDataDropped && !isDataReady) && (
+                <DropzoneWrapper setFiles={handleFileUpload} />
+            )}
+            {db && isDataDropped && !isDataReady && <Spinner />}
             {db && isDataReady && <Charts />}
         </>
     )
