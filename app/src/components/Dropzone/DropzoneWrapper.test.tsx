cf9849d6fa6933da525fce1ba229894bc26148cc
Gudsfile <gudsfile@protonmail.com>
2025-02-08 10:43:02 +0100

    add tests for dropzone components
    Co-authored-by: Tarektouati <19335073+Tarektouati@users.noreply.github.com>

---
+++ app/src/components/Dropzone/DropzoneWrapper.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import { it, vi } from 'vitest'
+
+import DropzoneWrapper from './DropzoneWrapper'
+
+it('should render', () => {
+    render(<DropzoneWrapper setFiles={vi.fn()} />)
+})
