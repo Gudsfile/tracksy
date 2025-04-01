9dd221b2051b61821eea3dd6abe4ce531da5a303
Gudsfile <gudsfile@protonmail.com>
2025-04-01 02:32:50 +0200

    feat(StreamPerHour): format chart to be a radar

--- app/src/components/Charts/StreamPerHour.test.tsx
+++ app/src/components/Charts/StreamPerHour.test.tsx
@@ -1,9 +1,23 @@
 import { describe, it, expect, vi } from 'vitest'
 import { render, screen, waitFor } from '@testing-library/react'
 import { StreamPerHour } from './StreamPerHour'
+import React from 'react'
+
+const queryResult = [
+    { username: 'alice', count_stream: 10, hour: 0 },
+    { username: 'alice', count_stream: 12, hour: 1 },
+    { username: 'alice', count_stream: 14, hour: 2 },
+    { username: 'alice', count_stream: 16, hour: 3 },
+    { username: 'alice', count_stream: 18, hour: 4 },
+    { username: 'alice', count_stream: 20, hour: 5 },
+    { username: 'alice', count_stream: 2, hour: 6 },
+    { username: 'alice', count_stream: 4, hour: 7 },
+    { username: 'bob', count_stream: 20, hour: 7 },
+    { username: 'bob', count_stream: 20, hour: 8 },
+]
 
 vi.mock('../../db/queries/queryDB', () => ({
-    queryDB: () => () => vi.fn(),
+    queryDB: () => () => queryResult,
 }))
 
 describe('StreamPerHour Component', () => {
@@ -11,9 +25,16 @@ describe('StreamPerHour Component', () => {
         const { container } = render(<StreamPerHour />)
 
         await waitFor(() => {
-            expect(container.querySelectorAll('svg')).toHaveLength(1)
+            expect(container.querySelectorAll('svg')).toHaveLength(3)
+            expect(
+                container.querySelectorAll('.stream-per-hour-hours-text text')
+            ).toHaveLength(9)
+            expect(
+                container.querySelectorAll('.stream-per-hour-points circle')
+            ).toHaveLength(10)
         })
-        screen.getByText('Nombre de streams')
-        screen.getByText('Heure (HH)')
+        screen.getByText('Streams per hour')
+        screen.getByText('alice')
+        screen.getByText('bob')
     })
 })
--- app/src/components/Charts/StreamPerHour.tsx
+++ app/src/components/Charts/StreamPerHour.tsx
@@ -2,11 +2,13 @@ import * as Plot from '@observablehq/plot'
 import { useState, useEffect, useRef } from 'react'
 import { queryDB } from '../../db/queries/queryDB'
 import { TABLE } from '../../db/queries/constants'
+import * as d3 from 'd3'
+import { Int } from 'apache-arrow'
 
 const query = `
 SELECT
-  count(*)::int count_stream,
-  hour(ts::datetime) as hour,
+  count(*)::int as count_stream,
+  hour(ts::datetime)::int as hour,
   username
 FROM ${TABLE}
 group by hour(ts::datetime), username
@@ -14,22 +16,84 @@ order by hour
 `
 
 function buildPlot(data: Awaited<ReturnType<typeof queryDB>>) {
+    const longitude = d3
+        .scalePoint(new Set(Plot.valueof(data, 'hour')), [180, -180])
+        .padding(0.5)
+        .align(1)
+    const maxCountStream = d3.max(data, (d) => d.count_stream)
+    const latitude = d3
+        .scaleLinear()
+        .domain([maxCountStream, 0])
+        .range([0.5, 0])
     return Plot.plot({
-        x: { label: 'Heure (HH)', type: 'band' },
-        y: { label: 'Nombre de streams', grid: true },
+        title: 'Streams per hour',
+        projection: {
+            type: 'azimuthal-equidistant',
+            rotate: [0, -90],
+            domain: d3.geoCircle().center([0, 90]).radius(0.625)(),
+        },
+        color: { legend: true },
         marks: [
-            Plot.ruleY([0]),
-            Plot.areaY(data, {
-                x: 'hour',
-                y: 'count_stream',
-                fillOpacity: 0.2,
+            // grey discs
+            Plot.geo([0.5, 0.4, 0.3, 0.2, 0.1], {
+                geometry: (r) => d3.geoCircle().center([0, 90]).radius(r)(),
+                stroke: 'black',
+                fill: 'black',
+                strokeOpacity: 0.3,
+                fillOpacity: 0.03,
+                strokeWidth: 0.5,
             }),
-            Plot.lineY(data, {
-                x: 'hour',
-                y: 'count_stream',
+            // white axes
+            Plot.link(longitude.domain(), {
+                x1: longitude,
+                y1: 90 - 0.57,
+                x2: 0,
+                y2: 90,
+                stroke: 'white',
+                strokeOpacity: 0.5,
+                strokeWidth: 2.5,
+            }),
+            // hours
+            Plot.text(longitude.domain(), {
+                x: longitude,
+                y: 90 - 0.57,
+                text: (d) => `${String(d).padStart(2, '0')}`,
+                lineWidth: 5,
+                className: 'stream-per-hour-hours-text',
+            }),
+            // areas
+            Plot.area(data, {
+                x1: ({ hour }) => longitude(hour),
+                y1: ({ count_stream }) => 90 - latitude(count_stream),
+                x2: 0,
+                y2: 90,
+                fill: 'username',
                 stroke: 'username',
-                tip: true,
+                curve: 'cardinal-closed',
+                fillOpacity: 0.3,
             }),
+            // points
+            Plot.dot(data, {
+                x: ({ hour }) => longitude(hour),
+                y: ({ count_stream }) => 90 - latitude(count_stream),
+                fill: 'username',
+                stroke: 'white',
+                className: 'stream-per-hour-points',
+            }),
+            // interactive labels
+            Plot.text(
+                data,
+                Plot.pointer({
+                    x: ({ hour }) => longitude(hour),
+                    y: ({ count_stream }) => 90 - latitude(count_stream),
+                    text: (d) => `${d.count_stream.toLocaleString()} stream(s)`,
+                    textAnchor: 'start',
+                    dx: 4,
+                    fill: 'currentColor',
+                    stroke: 'white',
+                    maxRadius: 10,
+                })
+            ),
         ],
     })
 }
