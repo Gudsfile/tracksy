7dfd6f5e7903d4b4ef67ef3c6420bb9165d5ec95
Gudsfile <gudsfile@protonmail.com>
2025-07-13 01:55:17 +0200

    feat(Data) merge synthetic-dataset project

---
+++ synthetic-datasets/synthetic_datasets/factories/spotify.py
@@ -0,0 +1,128 @@
+import random
+import string
+
+from faker import Faker
+from faker.providers import DynamicProvider
+
+from ..models.spotify import DEFAULT_TRACK_ID_SIZE, DEFAULT_TRACK_URI_SUFFIX, Album, Artist, Streaming, Track, User
+from .factory_config import FactoryConfig
+
+
+def init_faker() -> Faker:
+    fake = Faker()
+
+    reason_start_provider = DynamicProvider(
+        provider_name="reason_start",
+        elements=[
+            None,
+            "appload",
+            "clickrow",
+            "click-row",
+            "backbtn",
+            "fwdbtn",
+            "persisted",
+            "playbtn",
+            "remote",
+            "trackdone",
+            "trackerror",
+            "unknown",
+        ],
+    )
+    fake.add_provider(reason_start_provider)
+
+    reason_end_provider = DynamicProvider(
+        provider_name="reason_end",
+        elements=[
+            None,
+            "backbtn",
+            "clickrow",
+            "click-row",
+            "endplay",
+            "fwdbtn",
+            "logout",
+            "playbtn",
+            "remote",
+            "trackdone",
+            "trackerror",
+            "unexpected-exit",
+            "unexpected-exit-while-paused",
+            "unknown",
+        ],
+    )
+    fake.add_provider(reason_end_provider)
+    return fake
+
+
+def random_platforms(fake: Faker, nb_platforms: int) -> list[str]:
+    return [
+        fake.random_element(
+            [
+                fake.android_platform_token(),
+                fake.ios_platform_token(),
+                fake.linux_platform_token(),
+                fake.mac_platform_token(),
+                fake.windows_platform_token(),
+            ]
+        )
+        for _ in range(0, nb_platforms)
+    ]
+
+
+def random_artists(fake: Faker, nb_artists: int) -> list[Artist]:
+    return [Artist(name=fake.name()) for _ in range(nb_artists)]
+
+
+def random_albums(fake: Faker, nb_albums: int, artists) -> list[Album]:
+    return [
+        Album(name=fake.sentence(nb_words=3, variable_nb_words=True)[:-1], artist=random.choice(artists))
+        for _ in range(0, nb_albums)
+    ]
+
+
+def random_tracks(fake: Faker, nb_tracks: int, albums) -> list[Track]:
+    return [
+        Track(
+            uri=DEFAULT_TRACK_URI_SUFFIX
+            + "".join(random.choices(string.ascii_lowercase + string.digits, k=DEFAULT_TRACK_ID_SIZE)),
+            name=fake.sentence(nb_words=2, variable_nb_words=True)[:-1],
+            album=random.choice(albums),
+        )
+        for _ in range(0, nb_tracks)
+    ]
+
+
+def random_streaming(fake: Faker, track: Track, user: User) -> Streaming:
+    return Streaming(
+        ts=fake.date_time_between(start_date="-3y", end_date="now").isoformat(),
+        platform=random.choice(user.platforms),
+        ms_played=random.randint(0, 720000),
+        conn_country=fake.country_code(),
+        ip_addr_decrypted=fake.ipv4(),
+        user_agent_decrypted=fake.user_agent(),
+        master_metadata_track_name=track.name,
+        master_metadata_album_artist_name=track.album.artist.name,
+        master_metadata_album_album_name=track.album.name,
+        spotify_track_uri=track.uri,
+        episode_name=None,
+        episode_show_name=None,
+        spotify_episode_uri=None,
+        reason_start=fake.reason_start(),
+        reason_end=fake.reason_end(),
+        shuffle=bool(random.getrandbits(1)),
+        skipped=bool(random.getrandbits(1)),
+        offline=bool(random.getrandbits(1)),
+        offline_timestamp=None,
+        incognito_mode=False,
+    )
+
+
+def random_streamings(fake: Faker, nb_streams: int, user: User, tracks: list[Track]) -> list[Streaming]:
+    return [random_streaming(fake, random.choice(tracks), user) for _ in range(0, nb_streams)]
+
+
+def generate_streamings(fake: Faker, config: FactoryConfig) -> list[Streaming]:
+    user = User(platforms=random_platforms(fake, config.nb_platforms))
+    artists = random_artists(fake, config.nb_artists)
+    albums = random_albums(fake, config.nb_albums, artists)
+    tracks = random_tracks(fake, config.nb_tracks, albums)
+    return random_streamings(fake, config.nb_streams, user, tracks)
---
+++ synthetic-datasets/synthetic_datasets/hello.py
@@ -0,0 +1,57 @@
+import sys
+import time
+from pathlib import Path
+
+from tqdm import trange
+
+from .factories.factory_config import FactoryConfig
+from .factories.spotify import generate_streamings, init_faker
+from .writers.spotify import DEFAULT_EXTENSION, DEFAULT_FOLDER, DEFAULT_PREFIX, write, write_zip
+
+
+def spotify(nb_streams):
+    def generator(nb_streams):
+        print(nb_streams)
+        yield generate_streamings(
+            init_faker(),
+            FactoryConfig(
+                nb_streams=nb_streams,
+                nb_tracks=int(nb_streams * 0.5),
+                nb_albums=int(nb_streams * 0.3),
+                nb_artists=int(nb_streams * 0.2),
+            ),
+        )
+
+    print(f"Generating one file of {nb_streams} lines")
+    write(
+        next(generator(nb_streams)),
+        Path(f"datasets/{DEFAULT_FOLDER}/{DEFAULT_PREFIX}_2024-2025_{nb_streams}{DEFAULT_EXTENSION}"),
+    )
+
+    chunk_size = 20000
+    minimum_nb_file = 4
+    nb_file_to_write = max(int(nb_streams / chunk_size), minimum_nb_file)
+    minimum_chunk_size = 10
+    chunk_size = max(int(nb_streams / nb_file_to_write), minimum_chunk_size)
+    print(f"Generating {nb_file_to_write} files to be zipped")
+    write_zip(
+        [next(generator(chunk_size), minimum_chunk_size) for _ in trange(0, nb_file_to_write)],
+        Path(f"datasets/{DEFAULT_FOLDER}/"),
+    )
+
+
+def main():
+    args = sys.argv[1:]
+    if len(args) < 1 or len(args) > 1:
+        print(f"One and only one argument is required to set the number of lines to be generated. Given args: {args}")
+        sys.exit(1)
+    if not args[0].isdigit():
+        print(f"Number of lines to be generated should be integer. Given arg: {args[0]}")
+        sys.exit(1)
+    start_data_generation = time.time()
+    spotify(int(args[0]))
+    print("--- %s seconds ---" % (time.time() - start_data_generation))
+
+
+if __name__ == "__main__":
+    main()
