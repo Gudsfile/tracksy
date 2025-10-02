490f40c90a1c72c28000d2b7ace3954ad58f543d
Benjamin Frost <58258541+benjamin-frost@users.noreply.github.com>
2025-10-02 15:51:52 +0200

    test: add basic results tests

--- app/vitest.setupFiles.ts
+++ app/vitest.setupFiles.ts
@@ -4,6 +4,8 @@ import { afterEach, beforeEach } from 'vitest'
 
 import { DataTransferMock } from './src/fixtures/DataTransferMock'
 
+import '@testing-library/jest-dom/vitest'
+
 beforeEach(() => {
     // @ts-expect-error DataTransfer is not defined in jsdom
     global.DataTransfer = DataTransferMock
