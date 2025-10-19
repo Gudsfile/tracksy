573fe1c4a1a4057a4ef921763c9011acd9c4d1df
Gudsfile <gudsfile@protonmail.com>
2025-10-19 04:26:07 +0200

    chore: remove useless react import

--- app/src/components/Charts/StreamPerMonth/StreamPerMonth.test.tsx
+++ app/src/components/Charts/StreamPerMonth/StreamPerMonth.test.tsx
@@ -1,7 +1,6 @@
 import { describe, it, expect, vi } from 'vitest'
 import { render, screen, waitFor } from '@testing-library/react'
 import { StreamPerMonth } from '.'
-import React from 'react'
 import { ThemeProvider } from '../../../hooks/ThemeContext'
 
 vi.mock('../../../db/queries/queryDB', () => ({
--- app/src/components/Dropzone/DropzoneWrapper.test.tsx
+++ app/src/components/Dropzone/DropzoneWrapper.test.tsx
@@ -2,7 +2,6 @@ import { render } from '@testing-library/react'
 import { it, vi } from 'vitest'
 
 import { DropzoneWrapper } from './DropzoneWrapper'
-import React from 'react'
 
 it('should render', () => {
     render(<DropzoneWrapper setFiles={vi.fn()} />)
