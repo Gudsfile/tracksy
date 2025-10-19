e5c9f61bc5208f948e428fb826ef903f664d8517
Gudsfile <gudsfile@protonmail.com>
2025-10-19 04:17:48 +0200

    chore: reduce plot recomputation

--- app/src/components/Charts/StreamPerHour/index.tsx
+++ app/src/components/Charts/StreamPerHour/index.tsx
@@ -1,6 +1,8 @@
 import { type QueryResult, queryByYear } from './query'
 import { buildPlot } from './plot'
 import { Common } from '../Common'
+import { useCallback } from 'react'
+import { Table } from 'apache-arrow'
 
 interface StreamPerHourProps {
     year: number
@@ -8,10 +10,16 @@ interface StreamPerHourProps {
 }
 
 export function StreamPerHour({ year, maxValue }: StreamPerHourProps) {
+    const plotBuilder = useCallback(
+        (data: Table<QueryResult>, isDark: boolean | undefined) =>
+            buildPlot(data, maxValue, isDark),
+        [maxValue]
+    )
+
     return (
         <Common<QueryResult>
             query={queryByYear(year)}
-            buildPlot={(data, isDark) => buildPlot(data, maxValue, isDark)}
+            buildPlot={plotBuilder}
         />
     )
 }
--- app/src/components/Charts/SummaryPerYear/index.tsx
+++ app/src/components/Charts/SummaryPerYear/index.tsx
@@ -1,13 +1,18 @@
 import { type QueryResult, queryByYear } from './query'
 import { buildPlot } from './plot'
 import { Common } from '../Common'
+import { useCallback } from 'react'
 
 interface SummaryPerYearProps {
     year: number
 }
 
 export function SummaryPerYear({ year }: SummaryPerYearProps) {
+    const plotBuilder = useCallback(buildPlot, [])
     return (
-        <Common<QueryResult> query={queryByYear(year)} buildPlot={buildPlot} />
+        <Common<QueryResult>
+            query={queryByYear(year)}
+            buildPlot={plotBuilder}
+        />
     )
 }
