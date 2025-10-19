5d52ae92fcfb3144ab8f8c9c902b3381c55286c4
Gudsfile <gudsfile@protonmail.com>
2025-10-19 04:17:48 +0200

    chore: reduce plot recomputation

--- app/src/components/Charts/StreamPerMonth/index.tsx
+++ app/src/components/Charts/StreamPerMonth/index.tsx
@@ -1,13 +1,18 @@
 import { type QueryResult, queryByYear } from './query'
 import { buildPlot } from './plot'
 import { Common } from '../Common'
+import { useCallback } from 'react'
 
 interface StreamPerMonthProps {
     year: number
 }
 
 export function StreamPerMonth({ year }: StreamPerMonthProps) {
+    const plotBuilder = useCallback(buildPlot, [])
     return (
-        <Common<QueryResult> query={queryByYear(year)} buildPlot={buildPlot} />
+        <Common<QueryResult>
+            query={queryByYear(year)}
+            buildPlot={plotBuilder}
+        />
     )
 }
