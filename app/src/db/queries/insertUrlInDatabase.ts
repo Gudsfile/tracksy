eb4bd8288f7c250d92d659b8443530a85bf8a6f9
Gudsfile <gudsfile@protonmail.com>
2025-11-24 01:10:54 +0100

    feat: only keep streams that are at least 30 seconds long

--- app/src/db/queries/insertUrlInDatabase.ts
+++ app/src/db/queries/insertUrlInDatabase.ts
@@ -1,8 +1,13 @@
 import { getDB } from '../getDB'
-import { TABLE, DROP_TABLE_QUERY } from './constants'
+import {
+    TABLE,
+    DROP_TABLE_QUERY,
+    DELETE_SHORT_STREAMS_QUERY,
+} from './constants'
 
 export async function insertUrlInDatabase(jsonUrl: URL) {
     const { conn } = await getDB()
     await conn.query(DROP_TABLE_QUERY)
     await conn.insertJSONFromPath(jsonUrl.toString(), { name: TABLE })
+    await conn.query(DELETE_SHORT_STREAMS_QUERY)
 }
