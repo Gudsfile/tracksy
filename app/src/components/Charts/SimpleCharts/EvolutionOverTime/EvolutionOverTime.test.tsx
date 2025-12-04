2a013b06c0932ce3b6ff48f96b982e3b1dc64c13
Gudsfile <gudsfile@protonmail.com>
2025-12-04 22:34:03 +0100

    feat: add duration to evolution chart

--- app/src/components/Charts/SimpleCharts/EvolutionOverTime/EvolutionOverTime.test.tsx
+++ app/src/components/Charts/SimpleCharts/EvolutionOverTime/EvolutionOverTime.test.tsx
@@ -5,17 +5,17 @@ import { EvolutionOverTime } from './EvolutionOverTime'
 describe('EvolutionOverTime Component', () => {
     it('renders correctly with data', async () => {
         const data = [
-            { year: 2020, streams: 100 },
-            { year: 2021, streams: 150 },
-            { year: 2022, streams: 200 },
+            { year: 2020, streams: 100, ms_played: 60 * 60 * 1000 },
+            { year: 2021, streams: 150, ms_played: 60 * 60 * 1000 },
+            { year: 2022, streams: 200, ms_played: 60 * 60 * 1000 },
         ]
 
         render(<EvolutionOverTime data={data} />)
 
         screen.getByText('📈 Evolution')
-        screen.getByText('2020: 100')
-        screen.getByText('2021: 150')
-        screen.getByText('2022: 200')
+        screen.getByText('2020 100 streams (1h 0m 0s)')
+        screen.getByText('2021 150 streams (1h 0m 0s)')
+        screen.getByText('2022 200 streams (1h 0m 0s)')
         screen.getByText('Total streams')
         screen.getByText('450')
     })
