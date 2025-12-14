35940172f896415826d88b55b02e14f0241f4a1f
Tarektouati <19335073+Tarektouati@users.noreply.github.com>
2025-12-14 22:58:57 +0100

    test: remove vi.mock from TopArtists tests

--- app/src/components/Charts/TopArtists/TopArtists.test.tsx
+++ app/src/components/Charts/TopArtists/TopArtists.test.tsx
@@ -1,23 +1,27 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { render, screen, waitFor } from '@testing-library/react'
+
+import * as query from '../../../db/queries/queryDB'
+import * as db from '../../../db/getDB'
+import { TopArtistsQueryResult } from './query'
+
 import { TopArtists } from '.'
-
-vi.mock('../../../db/queries/queryDB', () => ({
-    queryDBAsJSON: () => () => [
-        {
-            artist_name: 'artist_a',
-            count_streams: 10n,
-            ms_played: 0n,
-        },
-    ],
-}))
-
-vi.mock('../../../db/getDB', () => ({
-    getDB: vi.fn(() => Promise.resolve({})),
-    insertFilesInDatabase: vi.fn(() => Promise.resolve()),
-}))
-
 describe('TopArtists Component', () => {
+    beforeEach(() => {
+        vi.spyOn(query, 'queryDBAsJSON').mockResolvedValue([
+            {
+                artist_name: 'artist_a',
+                count_streams: 10n,
+                ms_played: 0n,
+            },
+        ] as unknown as TopArtistsQueryResult[])
+
+        vi.spyOn(db, 'getDB').mockResolvedValue({
+            db: vi.fn(),
+            conn: vi.fn(),
+        } as unknown as Awaited<ReturnType<typeof db.getDB>>)
+    })
+
     it('should render the svg', async () => {
         const { container } = render(<TopArtists year={2006} />)
 
