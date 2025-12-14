5f7d71bca30ee39d99b19bf62478e5b70677bb85
Tarektouati <19335073+Tarektouati@users.noreply.github.com>
2025-12-14 22:58:53 +0100

    test: remove vi.mock from StreamPerMonth tests

--- app/src/components/Charts/StreamPerMonth/StreamPerMonth.test.tsx
+++ app/src/components/Charts/StreamPerMonth/StreamPerMonth.test.tsx
@@ -1,17 +1,23 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { render, waitFor } from '@testing-library/react'
+
+import * as query from '../../../db/queries/queryDB'
+import * as db from '../../../db/getDB'
+import { StreamPerMonthQueryResult } from './query'
+
 import { StreamPerMonth } from '.'
 
-vi.mock('../../../db/queries/queryDB', () => ({
-    queryDBAsJSON: () => () => vi.fn(),
-}))
-
-vi.mock('../../../db/getDB', () => ({
-    getDB: vi.fn(() => Promise.resolve({})),
-    insertFilesInDatabase: vi.fn(() => Promise.resolve()),
-}))
-
 describe('StreamPerMonth Component', () => {
+    beforeEach(() => {
+        vi.spyOn(query, 'queryDBAsJSON').mockResolvedValue(
+            [] as StreamPerMonthQueryResult[]
+        )
+        vi.spyOn(db, 'getDB').mockResolvedValue({
+            db: vi.fn(),
+            conn: vi.fn(),
+        } as unknown as Awaited<ReturnType<typeof db.getDB>>)
+    })
+
     it('should render the svg', async () => {
         const { container } = render(
             <StreamPerMonth year={2006} maxValue={100} />
