8c23473c5c899a7e25a3647c53d90d75c828ee3e
Tarektouati <19335073+Tarektouati@users.noreply.github.com>
2025-12-14 22:59:22 +0100

    test: remove vi.mock from TopTracks tests

--- app/src/components/Charts/TopTracks/TopTracks.test.tsx
+++ app/src/components/Charts/TopTracks/TopTracks.test.tsx
@@ -1,24 +1,31 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { render, screen, waitFor } from '@testing-library/react'
+
+import * as query from '../../../db/queries/queryDB'
+import * as db from '../../../db/getDB'
+import { TopTracksQueryResult } from './query'
+
 import { TopTracks } from '.'
 
-vi.mock('../../../db/queries/queryDB', () => ({
-    queryDBAsJSON: () => () => [
-        {
-            track_name: 'track_a',
-            artist_name: 'artist_b',
-            count_streams: 10n,
-            ms_played: 0n,
-        },
-    ],
-}))
-
-vi.mock('../../../db/getDB', () => ({
-    getDB: vi.fn(() => Promise.resolve({})),
-    insertFilesInDatabase: vi.fn(() => Promise.resolve()),
-}))
-
 describe('TopTracks Component', () => {
+    beforeEach(() => {
+        vi.spyOn(query, 'queryDBAsJSON').mockImplementation(() =>
+            Promise.resolve([
+                {
+                    track_name: 'track_a',
+                    artist_name: 'artist_b',
+                    count_streams: 10n,
+                    ms_played: 0n,
+                },
+            ] as unknown as TopTracksQueryResult[])
+        )
+
+        vi.spyOn(db, 'getDB').mockResolvedValue({
+            db: vi.fn(),
+            conn: vi.fn(),
+        } as unknown as Awaited<ReturnType<typeof db.getDB>>)
+    })
+
     it('should render the svg', async () => {
         const { container } = render(<TopTracks year={2006} />)
 
