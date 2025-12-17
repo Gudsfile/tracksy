b6dabf2fc8209b6549decbf34a12b47d88012c8b
Gudsfile <gudsfile@protonmail.com>
2025-12-17 13:38:36 +0100

    refactor: move ConcentrationScore sql query to an sql file

--- app/src/components/Charts/SimpleCharts/ConcentrationScore/query.tsx
+++ app/src/components/Charts/SimpleCharts/ConcentrationScore/query.tsx
@@ -1,4 +1,5 @@
 import { TABLE } from '../../../../db/queries/constants'
+import sqlQueryConcentrationScore from './ConcentrationScore.sql?raw'
 
 export type ConcentrationResult = {
     top5_pct: number
@@ -7,29 +8,7 @@ export type ConcentrationResult = {
 }
 
 export function queryConcentrationScore(year: number): string {
-    return `
-    WITH artist_streams AS (
-      SELECT
-        master_metadata_album_artist_name AS artist,
-        COUNT(*) AS stream_count
-      FROM ${TABLE}
-      WHERE master_metadata_album_artist_name IS NOT NULL
-      AND YEAR(ts::DATE) = ${year}
-      GROUP BY master_metadata_album_artist_name
-    ),
-    ranked_artists AS (
-      SELECT
-        artist,
-        stream_count,
-        ROW_NUMBER() OVER (ORDER BY stream_count DESC) AS rank
-      FROM artist_streams
-    ),
-    total_streams AS (
-      SELECT COUNT(*) AS total FROM ${TABLE} WHERE YEAR(ts::DATE) = ${year}
-    )
-    SELECT
-      (SELECT SUM(stream_count) FROM ranked_artists WHERE rank <= 5)::DOUBLE / (SELECT total FROM total_streams)::DOUBLE * 100 AS top5_pct,
-      (SELECT SUM(stream_count) FROM ranked_artists WHERE rank <= 10)::DOUBLE / (SELECT total FROM total_streams)::DOUBLE * 100 AS top10_pct,
-      (SELECT SUM(stream_count) FROM ranked_artists WHERE rank <= 20)::DOUBLE / (SELECT total FROM total_streams)::DOUBLE * 100 AS top20_pct
-  `
+    return sqlQueryConcentrationScore
+        .replaceAll('{{table}}', TABLE)
+        .replaceAll('{{year}}', String(year))
 }
