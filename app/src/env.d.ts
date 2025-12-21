6115f51e54a85d52dd86467cdac0301f955bab1a
bengeois <me@bbourgeois.dev>
2025-12-21 22:13:05 +0100

    feat(tests): add tests on Layout component

--- app/src/env.d.ts
+++ app/src/env.d.ts
@@ -1,2 +1,9 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+declare module '*.astro' {
+    import { Component } from 'astro'
+    const component: Component
+    // eslint-disable-next-line import/no-default-export
+    export default component
+}
--- app/vitest.setupFiles.ts
+++ app/vitest.setupFiles.ts
@@ -8,18 +8,21 @@ beforeEach(() => {
     // @ts-expect-error DataTransfer is not defined in jsdom
     global.DataTransfer = DataTransferMock
 
-    // Mock window.matchMedia for theme tests
-    Object.defineProperty(window, 'matchMedia', {
-        writable: true,
-        value: (query: string) => ({
-            matches: false,
-            media: query,
-            onchange: null,
-            addEventListener: () => {},
-            removeEventListener: () => {},
-            dispatchEvent: () => true,
-        }),
-    })
+    // HACK: Astro can compile component in multiple envs (browser & server), To be able to tests `*.astro` files we need executed them in node environment (server) where window doesn't exist
+    if (typeof window !== 'undefined') {
+        // Mock window.matchMedia for theme tests
+        Object.defineProperty(window, 'matchMedia', {
+            writable: true,
+            value: (query: string) => ({
+                matches: false,
+                media: query,
+                onchange: null,
+                addEventListener: () => {},
+                removeEventListener: () => {},
+                dispatchEvent: () => true,
+            }),
+        })
+    }
 })
 
 afterEach(() => {
