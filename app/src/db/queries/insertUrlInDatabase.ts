1c0d4afc3fba6c9c7bdf669416985fb118d32efd
Gudsfile <gudsfile@protonmail.com>
2026-03-01 14:21:57 +0100

    fix: apply model transformation in the demo workflow

--- app/src/db/queries/insertUrlInDatabase.ts
+++ app/src/db/queries/insertUrlInDatabase.ts
@@ -5,4 +5,12 @@ export async function insertUrlInDatabase(jsonUrl: URL) {
     const { conn } = await getDB()
     await conn.query(DROP_TABLE_QUERY)
     await conn.insertJSONFromPath(jsonUrl.toString(), { name: TABLE })
+    // The Spotify track URI is renamed to track_uri to match the `StreamRecord` model.
+    // `insertUrlInDatabase` is only used by the `DemoButton`, which provides a Spotify file.
+    // It's acceptable to apply this transformation here as a temporary hotfix, but ideally,
+    // we should use the transformation from `SpotifyStreamProvider` and automatically detect
+    // the correct provider to use.
+    await conn.query(
+        `ALTER TABLE ${TABLE} RENAME COLUMN spotify_track_uri TO track_uri`
+    )
 }
