024db7edd64c3a712d1ef7ab1fd47c581118cc8d
Gudsfile <gudsfile@protonmail.com>
2026-03-15 23:16:28 +0100

    refactor(charts): migrate SeasonalPatterns query to sql

---
+++ app/src/components/Charts/SimpleCharts/SeasonalPatterns/SeasonalPatterns.sql
@@ -0,0 +1,16 @@
+select
+    sum(
+        case when month(ts::date) in (12, 1, 2) then 1 else 0 end
+    )::double as winter,
+    sum(
+        case when month(ts::date) in (3, 4, 5) then 1 else 0 end
+    )::double as spring,
+    sum(
+        case when month(ts::date) in (6, 7, 8) then 1 else 0 end
+    )::double as summer,
+    sum(
+        case when month(ts::date) in (9, 10, 11) then 1 else 0 end
+    )::double as fall,
+    count(*)::double as total
+from ${table}
+where year(ts::date) = ${year}
