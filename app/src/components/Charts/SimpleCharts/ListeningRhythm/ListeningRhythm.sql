ab4f22554d7bc9c42d8793d66f80e91176b5b60c
Gudsfile <gudsfile@protonmail.com>
2026-03-15 23:17:12 +0100

    refactor(charts): migrate ListeningRhythm query to sql

---
+++ app/src/components/Charts/SimpleCharts/ListeningRhythm/ListeningRhythm.sql
@@ -0,0 +1,16 @@
+select
+    count(*) filter (
+        where hour(ts::timestamp) >= 6 and hour(ts::timestamp) < 12
+    )::double as morning,
+    count(*) filter (
+        where hour(ts::timestamp) >= 12 and hour(ts::timestamp) < 18
+    )::double as afternoon,
+    count(*) filter (
+        where hour(ts::timestamp) >= 18 and hour(ts::timestamp) < 22
+    )::double as evening,
+    count(*) filter (
+        where hour(ts::timestamp) >= 22 or hour(ts::timestamp) < 6
+    )::double as night,
+    count(*)::double as total
+from ${table}
+where year(ts::date) = ${year}
