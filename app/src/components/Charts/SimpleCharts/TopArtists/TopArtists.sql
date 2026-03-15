a46176eb19af1a27764018180d9707bc84c1a855
Gudsfile <gudsfile@protonmail.com>
2026-03-15 23:15:53 +0100

    refactor(charts): migrate TopArtists query to sql

---
+++ app/src/components/Charts/SimpleCharts/TopArtists/TopArtists.sql
@@ -0,0 +1,11 @@
+select
+    artist_name,
+    count(*)::double as count_streams,
+    sum(ms_played)::double as ms_played
+from ${table}
+where
+    artist_name is not null
+    and year(ts::date) = ${year}
+group by artist_name
+order by count_streams desc
+limit 5
--- app/src/components/Charts/SimpleCharts/TopArtists/query.tsx
+++ app/src/components/Charts/SimpleCharts/TopArtists/query.tsx
@@ -1,4 +1,5 @@
 import { TABLE } from '../../../../db/queries/constants'
+import sqlQueryTopArtists from './TopArtists.sql?raw'
 
 export type TopArtistsResult = {
     artist_name: string
@@ -7,16 +8,7 @@ export type TopArtistsResult = {
 }
 
 export function queryTopArtists(year: number): string {
-    return `
-    SELECT
-      artist_name AS artist_name,
-      COUNT(*)::DOUBLE AS count_streams,
-      SUM(ms_played)::DOUBLE AS ms_played
-    FROM ${TABLE}
-    WHERE artist_name IS NOT NULL
-      AND YEAR(ts::DATE) = ${year}
-    GROUP BY artist_name
-    ORDER BY count_streams DESC
-    LIMIT 5
-    `
+    return sqlQueryTopArtists
+        .replaceAll('${table}', TABLE)
+        .replaceAll('${year}', String(year))
 }
