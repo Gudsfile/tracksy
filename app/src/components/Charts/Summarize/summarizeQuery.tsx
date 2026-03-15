7b3e70f8ac58b5e615726080eaf33c9e488d6db7
Gudsfile <gudsfile@protonmail.com>
2026-03-15 23:04:25 +0100

    refactor(charts): migrate Summarize query to sql

--- app/src/components/Charts/Summarize/summarizeQuery.tsx
+++ app/src/components/Charts/Summarize/summarizeQuery.tsx
@@ -1,19 +1,7 @@
 import { TABLE } from '../../../db/queries/constants'
+import sqlQuerySummarize from './Summarize.sql?raw'
 
-export const summarizeQuery = `
-WITH monthly_aggregates AS (
-    SELECT SUM(ms_played) AS monthly_duration FROM ${TABLE} GROUP BY YEAR(ts::DATE), MONTH(ts::DATE)
-),
-hourly_aggregates AS (
-    SELECT COUNT(*) AS count_hourly_stream FROM ${TABLE} GROUP BY YEAR(ts::DATE), HOUR(ts::DATETIME)
-)
-SELECT
-    MIN(ts::DATETIME) AS min_datetime,
-    MAX(ts::DATETIME) AS max_datetime,
-    (SELECT MAX(count_hourly_stream) FROM hourly_aggregates) AS max_count_hourly_stream,
-    (SELECT MAX(monthly_duration) FROM monthly_aggregates) AS max_monthly_duration
-FROM ${TABLE};
-`
+export const summarizeQuery = sqlQuerySummarize.replaceAll('${table}', TABLE)
 
 export type SummarizeDataQueryResult = {
     min_datetime: string
