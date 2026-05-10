e162fe26ae262446378a04125603498892be4679
Gudsfile <gudsfile@protonmail.com>
2026-05-10 15:26:39 +0200

    feat(charts): add CalendarHeatmap container for SimpleCharts

---
+++ app/src/components/Charts/SimpleCharts/CalendarHeatmap/index.tsx
@@ -0,0 +1,19 @@
+import { useDBQueryMany } from '../../../../hooks/useDBQuery'
+import {
+    buildCalendarHeatmapQuery,
+    type CalendarHeatmapQueryResult,
+} from '../../DetailedCharts/CalendarHeatmap/query'
+import { CalendarHeatmap as CalendarHeatmapView } from './CalendarHeatmap'
+
+export function CalendarHeatmap({ year }: { year: number | undefined }) {
+    const { data, isLoading } = useDBQueryMany<CalendarHeatmapQueryResult>({
+        query: buildCalendarHeatmapQuery(year),
+        year,
+    })
+
+    if (year === undefined) {
+        return <CalendarHeatmapView data={undefined} year={undefined} />
+    }
+    if (!isLoading && !data) return null
+    return <CalendarHeatmapView data={data} year={year} isLoading={isLoading} />
+}
