996ad4e4311ce1e5951b43d966a9c9f4c6f50718
Gudsfile <gudsfile@protonmail.com>
2026-05-11 19:03:19 +0200

    refactor(funfacts): reformat test cases

--- app/src/components/Charts/SimpleCharts/FunFacts/queries.test.ts
+++ app/src/components/Charts/SimpleCharts/FunFacts/queries.test.ts
@@ -36,54 +36,18 @@ describe('FunFacts queries', () => {
 
     describe('Favorite artist queries', () => {
         const testData: TestStreamEntry[] = [
-            {
-                ts: '2024-02-07T06:00:00',
-                artist_name: 'top_morning_artist',
-            },
-            {
-                ts: '2024-02-07T07:00:00',
-                artist_name: 'top_morning_artist',
-            },
-            {
-                ts: '2024-02-07T08:00:00',
-                artist_name: 'morning_artist',
-            },
-            {
-                ts: '2024-02-07T12:00:00',
-                artist_name: 'top_afternoon_artist',
-            },
-            {
-                ts: '2024-02-07T13:00:00',
-                artist_name: 'afternoon_artist',
-            },
-            {
-                ts: '2024-02-07T14:00:00',
-                artist_name: 'top_afternoon_artist',
-            },
-            {
-                ts: '2024-02-07T18:00:00',
-                artist_name: 'evening_artist',
-            },
-            {
-                ts: '2024-02-07T19:00:00',
-                artist_name: 'top_evening_artist',
-            },
-            {
-                ts: '2024-02-07T20:00:00',
-                artist_name: 'top_evening_artist',
-            },
-            {
-                ts: '2024-02-07T00:00:00',
-                artist_name: 'top_night_artist',
-            },
-            {
-                ts: '2024-02-07T01:00:00',
-                artist_name: 'night_artist',
-            },
-            {
-                ts: '2024-02-07T02:00:00',
-                artist_name: 'top_night_artist',
-            },
+            { ts: '2024-02-07T06:00:00', artist_name: 'top_morning_artist' },
+            { ts: '2024-02-07T07:00:00', artist_name: 'top_morning_artist' },
+            { ts: '2024-02-07T08:00:00', artist_name: 'morning_artist' },
+            { ts: '2024-02-07T12:00:00', artist_name: 'top_afternoon_artist' },
+            { ts: '2024-02-07T13:00:00', artist_name: 'afternoon_artist' },
+            { ts: '2024-02-07T14:00:00', artist_name: 'top_afternoon_artist' },
+            { ts: '2024-02-07T18:00:00', artist_name: 'evening_artist' },
+            { ts: '2024-02-07T19:00:00', artist_name: 'top_evening_artist' },
+            { ts: '2024-02-07T20:00:00', artist_name: 'top_evening_artist' },
+            { ts: '2024-02-07T00:00:00', artist_name: 'top_night_artist' },
+            { ts: '2024-02-07T01:00:00', artist_name: 'night_artist' },
+            { ts: '2024-02-07T02:00:00', artist_name: 'top_night_artist' },
         ]
 
         beforeEach(async () => {
@@ -139,26 +103,11 @@ describe('FunFacts queries', () => {
 
     describe('Marathon queries', () => {
         const testData: TestStreamEntry[] = [
-            {
-                ts: '2024-01-01',
-                artist_name: 'consecutive_stream_artist',
-            },
-            {
-                ts: '2024-01-02',
-                artist_name: 'consecutive_stream_artist',
-            },
-            {
-                ts: '2024-01-03',
-                artist_name: 'consecutive_stream_artist',
-            },
-            {
-                ts: '2024-01-04',
-                artist_name: 'middle_stream_artist',
-            },
-            {
-                ts: '2024-01-05',
-                artist_name: 'consecutive_stream_artist',
-            },
+            { ts: '2024-01-01', artist_name: 'consecutive_stream_artist' },
+            { ts: '2024-01-02', artist_name: 'consecutive_stream_artist' },
+            { ts: '2024-01-03', artist_name: 'consecutive_stream_artist' },
+            { ts: '2024-01-04', artist_name: 'middle_stream_artist' },
+            { ts: '2024-01-05', artist_name: 'consecutive_stream_artist' },
         ]
 
         beforeEach(async () => {
@@ -179,10 +128,7 @@ describe('FunFacts queries', () => {
 
     describe('One hit wonder queries', () => {
         const testData: TestStreamEntry[] = [
-            {
-                artist_name: 'one_hit_wonder_artist',
-                track_name: 'hit_track',
-            },
+            { artist_name: 'one_hit_wonder_artist', track_name: 'hit_track' },
         ]
 
         beforeEach(async () => {
@@ -206,31 +152,13 @@ describe('FunFacts queries', () => {
     describe('Weekend favorite queries', () => {
         const testData: TestStreamEntry[] = [
             // Week
-            {
-                ts: '2025-12-01',
-                artist_name: 'week_artist',
-            },
-            {
-                ts: '2025-12-02',
-                artist_name: 'week_artist',
-            },
-            {
-                ts: '2025-12-03',
-                artist_name: 'weekend_artist',
-            },
-            // Week-end
-            {
-                ts: '2025-12-07',
-                artist_name: 'week_artist',
-            },
-            {
-                ts: '2025-12-07',
-                artist_name: 'weekend_artist',
-            },
-            {
-                ts: '2025-12-07',
-                artist_name: 'weekend_artist',
-            },
+            { ts: '2025-12-01', artist_name: 'week_artist' },
+            { ts: '2025-12-02', artist_name: 'week_artist' },
+            { ts: '2025-12-03', artist_name: 'weekend_artist' },
+            // Weekend
+            { ts: '2025-12-07', artist_name: 'week_artist' },
+            { ts: '2025-12-07', artist_name: 'weekend_artist' },
+            { ts: '2025-12-07', artist_name: 'weekend_artist' },
         ]
 
         beforeEach(async () => {
@@ -251,38 +179,14 @@ describe('FunFacts queries', () => {
 
     describe('Absolute loyalty queries', () => {
         const testData: TestStreamEntry[] = [
-            {
-                artist_name: 'loyal_artist',
-                reason_end: 'trackdone',
-            },
-            {
-                artist_name: 'loyal_artist',
-                reason_end: 'trackdone',
-            },
-            {
-                artist_name: 'loyal_artist',
-                reason_end: 'trackdone',
-            },
-            {
-                artist_name: 'loyal_artist',
-                reason_end: 'clickrow',
-            },
-            {
-                artist_name: 'artist1',
-                reason_end: 'fwdbtn',
-            },
-            {
-                artist_name: 'artist1',
-                reason_end: 'trackdone',
-            },
-            {
-                artist_name: 'artist1',
-                reason_end: 'trackdone',
-            },
-            {
-                artist_name: 'artist1',
-                reason_end: 'clickrow',
-            },
+            { artist_name: 'loyal_artist', reason_end: 'trackdone' },
+            { artist_name: 'loyal_artist', reason_end: 'trackdone' },
+            { artist_name: 'loyal_artist', reason_end: 'trackdone' },
+            { artist_name: 'loyal_artist', reason_end: 'clickrow' },
+            { artist_name: 'artist1', reason_end: 'fwdbtn' },
+            { artist_name: 'artist1', reason_end: 'trackdone' },
+            { artist_name: 'artist1', reason_end: 'trackdone' },
+            { artist_name: 'artist1', reason_end: 'clickrow' },
         ]
 
         beforeEach(async () => {
@@ -303,26 +207,11 @@ describe('FunFacts queries', () => {
 
     describe('Nostalgic return queries', () => {
         const testData: TestStreamEntry[] = [
-            {
-                ts: '2024-01-01',
-                artist_name: 'nostalgic_artist',
-            },
-            {
-                ts: '2024-01-01',
-                artist_name: 'nostalgic_artist',
-            },
-            {
-                ts: '2025-01-01',
-                artist_name: 'nostalgic_artist',
-            },
-            {
-                ts: '2025-01-01',
-                artist_name: 'artist1',
-            },
-            {
-                ts: '2025-01-01',
-                artist_name: 'artist2',
-            },
+            { ts: '2024-01-01', artist_name: 'nostalgic_artist' },
+            { ts: '2024-01-01', artist_name: 'nostalgic_artist' },
+            { ts: '2025-01-01', artist_name: 'nostalgic_artist' },
+            { ts: '2025-01-01', artist_name: 'artist1' },
+            { ts: '2025-01-01', artist_name: 'artist2' },
         ]
 
         beforeEach(async () => {
@@ -393,39 +282,16 @@ describe('FunFacts queries', () => {
     })
 
     describe('Current obsession queries', () => {
+        // prettier-ignore
         const testData: TestStreamEntry[] = [
             // Recent streams
-            {
-                ts: '2025-01-01',
-                artist_name: 'artist1',
-                track_name: 'current_hit',
-            },
-            {
-                ts: '2025-01-01',
-                artist_name: 'artist1',
-                track_name: 'current_hit',
-            },
-            {
-                ts: '2025-01-01',
-                artist_name: 'artist1',
-                track_name: 'track1',
-            },
+            { ts: '2025-01-01', artist_name: 'artist1', track_name: 'current_hit' },
+            { ts: '2025-01-01', artist_name: 'artist1', track_name: 'current_hit' },
+            { ts: '2025-01-01', artist_name: 'artist1', track_name: 'track1' },
             // Old streams
-            {
-                ts: '2024-01-01',
-                artist_name: 'artist1',
-                track_name: 'track1',
-            },
-            {
-                ts: '2024-01-01',
-                artist_name: 'artist1',
-                track_name: 'track1',
-            },
-            {
-                ts: '2024-01-01',
-                artist_name: 'artist1',
-                track_name: 'current_hit',
-            },
+            { ts: '2024-01-01', artist_name: 'artist1', track_name: 'track1' },
+            { ts: '2024-01-01', artist_name: 'artist1', track_name: 'track1' },
+            { ts: '2024-01-01', artist_name: 'artist1', track_name: 'current_hit' },
         ]
 
         beforeEach(async () => {
@@ -447,23 +313,11 @@ describe('FunFacts queries', () => {
     describe('Recent discovery queries', () => {
         const testData: TestStreamEntry[] = [
             // Recent streams
-            {
-                ts: '2025-01-01',
-                artist_name: 'recent_discovery_artist',
-            },
-            {
-                ts: '2025-01-01',
-                artist_name: 'already_discovered_artist',
-            },
-            {
-                ts: '2025-01-01',
-                artist_name: 'already_discovered_artist',
-            },
+            { ts: '2025-01-01', artist_name: 'recent_discovery_artist' },
+            { ts: '2025-01-01', artist_name: 'already_discovered_artist' },
+            { ts: '2025-01-01', artist_name: 'already_discovered_artist' },
             // Old streams
-            {
-                ts: '2024-01-01',
-                artist_name: 'already_discovered_artist',
-            },
+            { ts: '2024-01-01', artist_name: 'already_discovered_artist' },
         ]
 
         beforeEach(async () => {
@@ -484,18 +338,9 @@ describe('FunFacts queries', () => {
 
     describe('Subscribed artist queries', () => {
         const testData: TestStreamEntry[] = [
-            {
-                ts: '2024-01-01',
-                artist_name: 'subscribed_artist',
-            },
-            {
-                ts: '2024-02-01',
-                artist_name: 'subscribed_artist',
-            },
-            {
-                ts: '2024-03-01',
-                artist_name: 'subscribed_artist',
-            },
+            { ts: '2024-01-01', artist_name: 'subscribed_artist' },
+            { ts: '2024-02-01', artist_name: 'subscribed_artist' },
+            { ts: '2024-03-01', artist_name: 'subscribed_artist' },
             { ts: '2024-04-01', artist_name: 'artist1' },
             { ts: '2024-05-01', artist_name: 'artist1' },
             { ts: '2024-05-02', artist_name: 'artist1' },
@@ -520,22 +365,10 @@ describe('FunFacts queries', () => {
 
     describe('Old artist queries', () => {
         const testData: TestStreamEntry[] = [
-            {
-                ts: '2006-01-01',
-                artist_name: 'first_artist',
-            },
-            {
-                ts: '2008-01-01',
-                artist_name: 'second_artist',
-            },
-            {
-                ts: '2018-01-01',
-                artist_name: 'second_artist',
-            },
-            {
-                ts: '2024-01-01',
-                artist_name: 'first_artist',
-            },
+            { ts: '2006-01-01', artist_name: 'first_artist' },
+            { ts: '2008-01-01', artist_name: 'second_artist' },
+            { ts: '2018-01-01', artist_name: 'second_artist' },
+            { ts: '2024-01-01', artist_name: 'first_artist' },
         ]
 
         beforeEach(async () => {
@@ -596,27 +429,12 @@ describe('FunFacts queries', () => {
     describe('Forgotten artist queries', () => {
         const testData: TestStreamEntry[] = [
             // Old streams
-            {
-                ts: '2024-01-01',
-                artist_name: 'forgotten_artist',
-            },
-            {
-                ts: '2024-01-01',
-                artist_name: 'old_artist',
-            },
+            { ts: '2024-01-01', artist_name: 'forgotten_artist' },
+            { ts: '2024-01-01', artist_name: 'old_artist' },
             // Recent streams
-            {
-                ts: '2025-01-01',
-                artist_name: 'old_artist',
-            },
-            {
-                ts: '2025-01-01',
-                artist_name: 'new_artist',
-            },
-            {
-                ts: '2025-01-01',
-                artist_name: 'new_artist',
-            },
+            { ts: '2025-01-01', artist_name: 'old_artist' },
+            { ts: '2025-01-01', artist_name: 'new_artist' },
+            { ts: '2025-01-01', artist_name: 'new_artist' },
         ]
 
         beforeEach(async () => {
@@ -639,14 +457,8 @@ describe('FunFacts queries', () => {
 
     describe('Track proposition queries', () => {
         const testData: TestStreamEntry[] = [
-            {
-                artist_name: 'artist1',
-                track_name: 'track1',
-            },
-            {
-                artist_name: 'artist2',
-                track_name: 'track2',
-            },
+            { artist_name: 'artist1', track_name: 'track1' },
+            { artist_name: 'artist2', track_name: 'track2' },
         ]
 
         beforeEach(async () => {
@@ -684,7 +496,7 @@ describe('FunFacts queries', () => {
                 { artist_name: 'artist1', track_name: 'track5', album_name: 'album1', ts: RECENT_SUNDAY },
                 { artist_name: 'artist1', track_name: 'track6', album_name: 'album1', ts: RECENT_SUNDAY },
                 { artist_name: 'artist1', track_name: 'track7', album_name: 'album1', ts: RECENT_SUNDAY },
-          ]
+            ]
 
             it('contains album and artist names', async () => {
                 await createTestTable(conn, testData)
@@ -710,10 +522,9 @@ describe('FunFacts queries', () => {
                 { artist_name: 'artist1', album_name: 'album1', track_name: 't2', ts: RECENT_SUNDAY },
                 { artist_name: 'artist1', album_name: 'album1', track_name: 't3', ts: RECENT_SUNDAY },
                 { artist_name: 'artist1', album_name: 'album1', track_name: 't4', ts: RECENT_SUNDAY },
-                { artist_name: 'artist1', album_name: 'album1', track_name: 't5', ts: RECENT_MON_2AM },  // Monday 2am (valid)
-                { artist_name: 'artist1', album_name: 'album1', track_name: 't6', ts: RECENT_MON_3AM },  // Monday 3am (valid)
+                { artist_name: 'artist1', album_name: 'album1', track_name: 't5', ts: RECENT_MON_2AM },
+                { artist_name: 'artist1', album_name: 'album1', track_name: 't6', ts: RECENT_MON_3AM },
                 { artist_name: 'artist1', album_name: 'album1', track_name: 't7', ts: RECENT_SUNDAY },
-
                 // album2 -> 8 distinct tracks but Monday 6am (invalid)
                 { artist_name: 'artist2', album_name: 'album2', track_name: 'a1', ts: RECENT_MON_6AM },
                 { artist_name: 'artist2', album_name: 'album2', track_name: 'a2', ts: RECENT_MON_6AM },
