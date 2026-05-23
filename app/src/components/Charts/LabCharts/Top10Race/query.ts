e9887035bf5ec67920ad235da6d5ed5b5f5da78c
Gudsfile <gudsfile@protonmail.com>
2026-05-23 18:16:56 +0200

    refactor(charts): rename SQL files to include Artists for consistency

--- app/src/components/Charts/LabCharts/Top10Race/query.ts
+++ app/src/components/Charts/LabCharts/Top10Race/query.ts
@@ -1,5 +1,5 @@
 import { TABLE } from '../../../../db/queries/constants'
-import sqlTop10Race from './Top10Race.sql?raw'
+import sqlTop10Race from './Top10Artists.sql?raw'
 import sqlTop10Tracks from './Top10Tracks.sql?raw'
 import sqlTop10Albums from './Top10Albums.sql?raw'
 
