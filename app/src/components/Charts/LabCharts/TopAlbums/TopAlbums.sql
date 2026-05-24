97688032fb6a85bd3f2d93173a12dfd12fe77a52
Gudsfile <gudsfile@protonmail.com>
2026-05-24 03:29:21 +0200

    chore(charts): remove TopAlbums from Lab view
    TopAlbums is already available in the simple view.

--- app/src/components/Charts/LabCharts/TopAlbums/TopAlbums.sql
+++
@@ -1,9 +0,0 @@
-select
-    album_name,
-    count(*)::double as count_streams,
-    sum(ms_played)::double as ms_played
-from ${table}
-where ${year_condition}
-group by album_name
-order by count_streams desc
-limit 10
