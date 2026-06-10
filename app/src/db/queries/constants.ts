64f6f622ed515c2fa04618cc39e76521008648b6
Gudsfile <gudsfile@protonmail.com>
2026-06-11 01:54:44 +0200

    refactor(db): inline DROP TABLE query, remove DROP_TABLE_QUERY constant

--- app/src/db/queries/constants.ts
+++ app/src/db/queries/constants.ts
@@ -1,7 +1,5 @@
 export const TABLE = 'music_streams'
 
-export const DROP_TABLE_QUERY = `DROP TABLE IF EXISTS ${TABLE}`
-
 export const DAILY_STREAM_COUNTS_TABLE = 'daily_stream_counts'
 export const ARTIST_FIRST_YEAR_TABLE = 'artist_first_year'
 export const STREAM_SESSIONS_TABLE = 'stream_sessions'
